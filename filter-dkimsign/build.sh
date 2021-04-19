1378dd3f217a43c1168ed01a5797bfd3e0fed2b2
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2021-04-19 13:00:04 +0800

    Define _GNU_SOURCE to fix compile errors

--- filter-dkimsign/build.sh
+++ filter-dkimsign/build.sh
@@ -17,7 +17,7 @@ apply_patch() {
 }
 
 make_libopensmtpd() {
-    local MAKE="NEED_OPENBSD_COMPAT=1 NEED_RECALLOCARRAY=1 NEED_STRLCAT=1 NEED_STRLCPY=1 NEED_STRTONUM=1 make -C 'libopensmtpd-${LIBOPENSMTPD_VERSION}' -f Makefile.gnu"
+    local MAKE="-D_GNU_SOURCE NEED_OPENBSD_COMPAT=1 NEED_RECALLOCARRAY=1 NEED_STRLCAT=1 NEED_STRLCPY=1 NEED_STRTONUM=1 make -C 'libopensmtpd-${LIBOPENSMTPD_VERSION}' -f Makefile.gnu"
     local INSTALL="install -o root -g root -m 644"
     local INCLUDE_PATH="/usr/include/"
 
